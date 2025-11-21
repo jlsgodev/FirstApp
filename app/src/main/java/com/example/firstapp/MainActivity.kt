4499d1d7eb41a68c8e83b8432b9e69d787835cb0
jlsgodev <jlsgo.dev@outlook.com>
2025-11-20 22:39:51 -0300

    Primeira versão do projeto

---
+++ app/src/main/java/com/example/firstapp/MainActivity.kt
@@ -0,0 +1,28 @@
+package com.example.firstapp
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            Greeting("Android")
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String) {
+    Text(text = "Oi $name!")
+}
+
+@Preview(showBackground = true)
+@Composable
+fun DefaultPreview() {
+    Greeting("Android")
+}
